d4098adaf26c18bdcc727499d4661dfa92d5528f
ndphuong0712 <dongphuong07122003@gmail.com>
2023-04-15 18:23:10 +0700

    update

--- ThongBao.js
+++ ThongBao.js
@@ -28,3 +28,24 @@ previous.addEventListener('click',function(){
     thongTin.style.transform = `translateX(${pos}%)`
     check()
 })
+
+
+// sub container
+var backgroundSubContainer = document.querySelector('.background-sub-container')
+var tieuDe_SubContainer= document.querySelector('.sub-container .tieu-de')
+var noiDung_SubContainer = document.querySelector('.sub-container p')
+var close = document.querySelector('.close-sub-container')
+var xemChiTiet = document.querySelectorAll('.chi-tiet span')
+var mangXemChiTiet = [...xemChiTiet]
+close.onclick = function(){
+    backgroundSubContainer.classList.remove('open-sub-container')
+}
+mangXemChiTiet.forEach(function(node){
+    node.addEventListener('click',function(){
+        backgroundSubContainer.classList.add('open-sub-container')
+        var tieuDe = node.parentElement.parentElement.querySelector('.tieu-de').innerHTML
+        var noiDung = node.parentElement.parentElement.querySelector('.noi-dung').innerHTML
+        tieuDe_SubContainer.innerHTML=tieuDe
+        noiDung_SubContainer.innerHTML=noiDung
+    })
+})
